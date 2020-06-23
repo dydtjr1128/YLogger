fba08878611f6ab051bb20944c2028a10ecfb480
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-23 16:54:04 +0900

    Add: Add YLogger example

--- YLogger/Main.cpp
+++ YLogger/Main.cpp
@@ -3,5 +3,18 @@
 #include "YLogger.h"
 
 int main() {
-    std::cout << "Hello World!\n";
+	system("chcp 949");
+	logger::YLogger logger;
+	logger.Info("로그 1 입니다.");
+	
+	for (int i = 0; i < 1000; i++) {
+		std::thread tt([i,&logger]() {			
+			if(i%2 == 0)
+				logger.Info("로그" + std::to_string(i));
+			else
+				logger.Debug("로그" + std::to_string(i));
+			});
+		tt.detach();
+	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(100000));
 }
