6db861022ea48d01fb5f242cd5bde678b846cf0d
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-23 12:30:24 +0900

    Update codes to single header

--- YLogger/Main.cpp
+++ YLogger/Main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 
+#include "YLogger.h"
+
 int main() {
     std::cout << "Hello World!\n";
 }
--- YLogger/YLogger.h
+++ YLogger/YLogger.h
@@ -1,14 +1,75 @@
 #ifndef YLOGGER_H
 #define YLOGGER_H
 
+#include <chrono>
 #include <filesystem>
+#include <functional>
+#include <thread>
+#include <unordered_map>
+
 namespace logger {
+
+	namespace {
+		constexpr size_t kDefaultFileWatchingTime = 1000;
+	}
+
+	enum class LoggerType {
+		ConsoleAppender,
+		FileAppender,
+		RollingFileAppender
+	};
+
+
 	class YLogger {
 	public:
-		explicit YLogger(std::filesystem::path path = std::filesystem::current_path());
-		~YLogger();
+		explicit YLogger(std::filesystem::path path = std::filesystem::current_path()) : path_(path) {}
+		~YLogger() {}
 	private:
 		std::filesystem::path path_;
+	};
+
+
+	enum class FileStatus {
+		Created,
+		Modified,
+		Erased
+	};
+
+	class FileWatchar {
+	public:
+		FileWatchar() :running_(true) {}
+		void start(std::function<void(std::filesystem::path, FileStatus)> function) {
+			while (running_) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(kDefaultFileWatchingTime));
+
+				for (auto& it : file_watching_map_) {
+					if (std::filesystem::exists(it.first) == false) {
+						function(it.first, FileStatus::Erased);
+					}
+					else if (it.second == std::filesystem::file_time_type::min() && std::filesystem::last_write_time(it.first) > it.second()) {
+						function(it.first, FileStatus::Created);
+					}
+					else  {
+						function(it.first, FileStatus::Modified);
+					}
+				}
+
+			}
+		}
+		void addPath(std::filesystem::path path) {
+			if (std::filesystem::exists(path)) {				
+				file_watching_map_[path] = std::filesystem::last_write_time(path);
+			}
+			else {
+				file_watching_map_[path] = std::filesystem::file_time_type::min();
+			}
+		}
+	private:
+		std::unordered_map<std::filesystem::path, std::filesystem::file_time_type> file_watching_map_;
+		bool running_;
+		bool IsContain(const std::string& key) {
+			return file_watching_map_.find(key) != file_watching_map_.end();
+		}
 	};
 }
 
