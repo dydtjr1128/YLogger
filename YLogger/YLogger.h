a3236d5dca337fc3b58a111c991709257e6bcba6
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-24 18:51:48 +0900

    Update: Update logger to define & append file name, line, function

--- YLogger/YLogger.h
+++ YLogger/YLogger.h
@@ -14,9 +14,6 @@
 #include <unordered_map>
 #include <vector>
 
-
-
-
 namespace logger {
 
 	namespace {
@@ -57,7 +54,72 @@ namespace logger {
 
 	class ConsoleAppender : public Appender {
 	public:
+
 		void action(const Log& log) {
+
+			std::cout << log.log();
+		}
+	private:
+	};
+
+	class YLogger {
+	public:
+		static YLogger* getLogger() {
+			if (logger == nullptr)
+				logger = new YLogger();
+			return logger;
+		}
+		explicit YLogger(std::filesystem::path savePath = std::filesystem::current_path()) : savePath_(savePath), finish_(false), logLevel_(LogLevel::Debug) {
+			loggingThread_ = std::thread([&] {
+				while (true) {
+					std::unique_lock lock(logMutex_);
+					log_cv.wait(lock, [&] {return !logQueue_.empty() || finish_; });
+
+					if (logQueue_.empty() && finish_) {
+						break;
+					}
+
+					const Log log = logQueue_.front();
+					logQueue_.pop();
+					lock.unlock();
+
+					for (const auto& appender : logTypeVector_) {
+						appender->action(log);
+					}
+				}
+				});
+			ReadConfig();
+			std::thread t([&]() {
+				while (true) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+					std::cout << "@@" << logQueue_.size() << std::endl;
+				}
+				});
+			t.detach();
+		}
+		~YLogger() {
+			std::cout << logQueue_.size() << std::endl;
+			finish_ = true;
+			log_cv.notify_all();
+			loggingThread_.join();
+			std::cout << logQueue_.size() << std::endl;
+		}
+
+		//void Trace(std::string log) { pushLog(LogLevel::Trace, log); };
+		//void Debug(std::string log) { pushLog(LogLevel::Debug, log); };
+		//void Info(std::string log) { pushLog(LogLevel::Info, log); };
+		//void Warn(std::string log) { pushLog(LogLevel::Warn, log); };
+		//void Error(std::string log) { pushLog(LogLevel::Error, log); };
+		//void Fatal(std::string log) { pushLog(LogLevel::Trace, log); };
+
+		//void pushLog(LogLevel level, const std::string& log) {
+		//	std::unique_lock lock(logMutex_);
+
+		//	logQueue_.emplace(level, log);
+		//}
+
+		void pushLog(LogLevel level, std::string file, std::string func, int line, std::string log) {
+			std::unique_lock lock(logMutex_);
 			oss_.str("");
 			oss_.clear();
 
@@ -68,10 +130,43 @@ namespace logger {
 			std::strftime(&time[0], time.size(), "%H:%M:%S", &localtime(nowTime));
 			auto milliseconds = now - std::chrono::time_point_cast<std::chrono::seconds>(now);
 
-			oss_ << "[" << std::setw(5) << std::setfill(' ') << logLevel[(int)log.level()] << "] [" << __func__ << "] [" << time << "." << std::setw(3) << std::setfill('0') << milliseconds.count() / 10000 << "] : " << log.log() << std::endl;
-			std::cout << oss_.str();
+			oss_ << "[" << std::setw(5) << std::setfill(' ') << logLevel[(int)level] << "] [" << func << " at " << file << "::" << line << "] [" << time << "." << std::setw(3) << std::setfill('0') << milliseconds.count() / 10000 << "] : " << log << std::endl;
+			logQueue_.emplace(level, oss_.str());
+			log_cv.notify_one();
 		}
+
+		//void print() {
+		//	while (true) {
+		//		std::unique_lock lock(logMutex_);
+		//		log_cv.wait(lock, [&] {return !logQueue_.empty() || finish_; });
+
+		//		const Log& log = logQueue_.front();
+		//		logQueue_.pop();
+		//		lock.unlock();
+
+		//		for (const auto& appender : logTypeVector_) {
+		//			appender->action(log);
+		//		}
+		//	}
+		//}
+
+		LogLevel GetLogLevel() {
+			return logLevel_;
+		}
+
+		static inline YLogger* logger = nullptr;
 	private:
+
+
+		std::thread loggingThread_;
+		std::queue<Log> logQueue_;
+		std::vector<std::unique_ptr<Appender>> logTypeVector_;
+		std::filesystem::path savePath_;
+		std::mutex logMutex_;
+		std::condition_variable log_cv;
+
+		bool finish_;
+		LogLevel logLevel_;
 		std::ostringstream oss_;
 
 		inline std::tm localtime(std::time_t timer)
@@ -88,72 +183,7 @@ namespace logger {
 #endif
 			return bt;
 		}
-	};
 
-	class YLogger {
-	public:
-		explicit YLogger(std::filesystem::path savePath = std::filesystem::current_path()) : savePath_(savePath), finish_(false) {
-			loggingThread_ = std::thread([&] {
-				while (true) {
-					std::unique_lock lock(logMutex_);
-					log_cv.wait(lock, [&] {return !logQueue_.empty() || finish_; });
-
-					if (logQueue_.empty() && finish_)
-						break;
-
-					const Log log = logQueue_.front();
-					logQueue_.pop();
-					lock.unlock();
-
-					for (const auto& appender : logTypeVector_) {
-						appender->action(log);
-					}
-				}
-				});
-			ReadConfig();
-		}
-		~YLogger() {
-			finish_ = true;
-			log_cv.notify_all();
-			loggingThread_.join();
-		}
-
-		void Trace(std::string log) { pushLog(LogLevel::Trace, log); };
-		void Debug(std::string log) { pushLog(LogLevel::Debug, log); };
-		void Info(std::string log) { pushLog(LogLevel::Info, log); };
-		void Warn(std::string log) { pushLog(LogLevel::Warn, log); };
-		void Error(std::string log) { pushLog(LogLevel::Error, log); };
-		void Fatal(std::string log) { pushLog(LogLevel::Trace, log); };
-
-		void pushLog(LogLevel level, const std::string& log) {
-			std::unique_lock lock(logMutex_);
-
-			logQueue_.emplace(level, log);
-		}
-
-		void print() {
-			while (true) {
-				std::unique_lock lock(logMutex_);
-				log_cv.wait(lock, [&] {return !logQueue_.empty() || finish_; });
-
-				const Log& log = logQueue_.front();
-				logQueue_.pop();
-				lock.unlock();
-
-				for (const auto& appender : logTypeVector_) {
-					appender->action(log);
-				}
-			}
-		}
-	private:
-		std::thread loggingThread_;
-		std::queue<Log> logQueue_;
-		std::vector<std::unique_ptr<Appender>> logTypeVector_;
-		std::filesystem::path savePath_;
-		std::mutex logMutex_;
-		std::condition_variable log_cv;
-
-		bool finish_;
 
 		void ReadConfig() {
 			//파일 읽어서 설정 보고 맞는 appender 객체 생성 및 추가
@@ -161,6 +191,13 @@ namespace logger {
 			logTypeVector_.emplace_back(std::make_unique<ConsoleAppender>());
 		}
 	};
+	inline logger::YLogger* getLogger() { return logger::YLogger::getLogger(); }
+#define LOG_TRACE(log) { logger::YLogger* logger = logger::getLogger(); if (logger != NULL)																  logger->pushLog(logger::LogLevel::Trace,  __FILE__,  __func__, __LINE__, log); }
+#define LOG_DEBUG(log) { logger::YLogger* logger = logger::getLogger(); if (logger != NULL && (int)logger->GetLogLevel() <= (int)logger::LogLevel::Debug) logger->pushLog(logger::LogLevel::Debug,  __FILE__,  __func__, __LINE__, log); }
+#define LOG_INFO(log)  { logger::YLogger* logger = logger::getLogger(); if (logger != NULL && (int)logger->GetLogLevel() <= (int)logger::LogLevel::Info ) logger->pushLog(logger::LogLevel::Info ,  __FILE__,  __func__, __LINE__, log); }
+#define LOG_WARN(log)  { logger::YLogger* logger = logger::getLogger(); if (logger != NULL && (int)logger->GetLogLevel() <= (int)logger::LogLevel::Warn ) logger->pushLog(logger::LogLevel::Warn ,  __FILE__,  __func__, __LINE__, log); }
+#define LOG_ERROR(log) { logger::YLogger* logger = logger::getLogger(); if (logger != NULL && (int)logger->GetLogLevel() <= (int)logger::LogLevel::Error) logger->pushLog(logger::LogLevel::Error,  __FILE__,  __func__, __LINE__, log); }
+#define LOG_FATAL(log) { logger::YLogger* logger = logger::getLogger(); if (logger != NULL && (int)logger->GetLogLevel() <= (int)logger::LogLevel::Fatal) logger->pushLog(logger::LogLevel::Fatal,  __FILE__,  __func__, __LINE__, log); }
 
 
 	enum class FileStatus {
