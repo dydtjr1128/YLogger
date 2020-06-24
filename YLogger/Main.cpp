ed14ead3c836a88b71e691d21e903ce193dd0c47
Choi Yong Seok <dydtjr1994@gmail.com>
2020-06-24 18:52:02 +0900

    Update: Update YLogger example

--- YLogger/Main.cpp
+++ YLogger/Main.cpp
@@ -2,19 +2,27 @@
 
 #include "YLogger.h"
 
+void func() {
+
+}
+
 int main() {
 	system("chcp 949");
-	logger::YLogger logger;
-	logger.Info("로그 1 입니다.");
-	
+	//logger::YLogger logger;
+	LOG_DEBUG("로그 1 입니다.");
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	for (int i = 0; i < 1000; i++) {
-		std::thread tt([i,&logger]() {			
-			if(i%2 == 0)
-				logger.Info("로그" + std::to_string(i));
-			else
-				logger.Debug("로그" + std::to_string(i));
+		std::thread tt([i]() {			
+			if (i % 2 == 0) {
+				LOG_DEBUG("로그" + std::to_string(i));
+				//logger.Info("로그" + std::to_string(i));
+			}
+			else {
+				LOG_INFO("로그" + std::to_string(i));
+				//logger.Debug("로그" + std::to_string(i));
+			}
 			});
 		tt.detach();
 	}
-	std::this_thread::sleep_for(std::chrono::milliseconds(100000));
+	std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 }
