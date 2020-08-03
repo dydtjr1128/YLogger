2d487bc3605690611f234c9c14a3e77207a1bc6b
Choi Yong Seok <dydtjr1994@gmail.com>
2020-08-03 17:39:33 +0900

    Update: update example

--- YLogger/Main.cpp
+++ YLogger/Main.cpp
@@ -1,5 +1,10 @@
 ﻿#include "YLogger.h" // only include single header file
 
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
 void func1(int i) {
 	LOG_DEBUG("로그 - " + std::to_string(i));
 }
@@ -9,25 +14,23 @@ void func2(int i) {
 }
 
 int main() {
-	//system("chcp 949");
+	system("chcp 949");
 	//logger::YLogger::Initialize(); // Default config, Not essential. default is logger::LogLevel::Debug
 	logger::YLogger::Initialize(logger::LogLevel::Info); // Set logger level
 	logger::YLogger::GetLogger()->AddLogger(logger::LoggerType::ConsoleAppender);
 	logger::YLogger::GetLogger()->AddLogger(logger::LoggerType::FileAppender, "./temp2", "Hello");
 
-	std::cout << "Write 100 lines" << std::endl;
-
-	for (int i = 0; i < 100; i++) {
-		std::thread tt([i]() {
-			// thread-safe log
+	while (true) {
+		std::cout << "Write 10000 lines" << std::endl;
+		for (int i = 0; i < 10000; i++) {
 			if (i % 2 == 0) {
 				func1(i);
 			}
 			else {
 				func2(i);
 			}
-			});
-		tt.detach();
+			std::this_thread::sleep_for(10s);
+		}
 	}
 
 	return 0;
